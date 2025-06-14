ff48a02ac551abf0593489b57a8ffb9e022c6c74
dooly <doolanl208@gmail.com>
2025-06-14 14:58:39 +1000

    working on host mode

--- Basis/Packages/com.basis.framework/Networking/BasisNetworkServerRunner.cs
+++ Basis/Packages/com.basis.framework/Networking/BasisNetworkServerRunner.cs
@@ -13,18 +13,13 @@ public class BasisNetworkServerRunner
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Initalize(Configuration Configuration, string LogPath)
     {
-        //  BasisPrometheus.StartPrometheus(config.PromethusPort, config.PromethusUrl);
-        // Initialize server-side logging
-       //string FolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs");
         BasisServerSideLogging.Initialize(Configuration, LogPath);
-        // Create a cancellation token source
         cancellationTokenSource = new CancellationTokenSource();
         var cancellationToken = cancellationTokenSource.Token;
         serverTask = Task.Run(() =>
         {
             try
             {
-                Configuration Configuration = new Configuration();
                 NetworkServer.StartServer(Configuration);
             }
             catch (Exception ex)
